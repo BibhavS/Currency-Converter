32e7d3295603751cc45bd9cfc293f8f215dd4428
Bibhav Shrestha <bibhavshrestha60@gmail.com>
2024-03-01 21:51:33 +0545

    First commmit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react' 
+import './App.css'
+
+function App() {
+  return (
+    <>
+       <h1 className='text-3xl font-medium text-indigo-800 text-center mt-6'>Currency Converter</h1>
+    </>
+  )
+}
+
+export default App
