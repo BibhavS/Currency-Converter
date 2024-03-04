3c3f45aec3c07df7698f88d222e425e596fb43b3
Bibhav Shrestha <bibhavshrestha60@gmail.com>
2024-03-04 16:29:13 +0545

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,64 @@ import { InputBox } from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 function App() {
-  const [amount, setAmount] = useState();
+  const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState('usd');
   const [to, setTo] = useState('npr');
   const [convertedAmount, setConvertedAmount] = useState(0);
 
   const currencyInfo = useCurrencyInfo(from);
   const options = Object.keys(currencyInfo);
-
-  const swap = () =>{
-    setFrom(to);
-    setTo(from);
-    setConvertedAmount(amount);
-    setAmount(convertedAmount);
-  }
-
+  
   const convert = () =>{
     setConvertedAmount(amount * currencyInfo[to]);
   }
+  
+  const swap = () =>{
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(0);
+    setAmount(0);
+  }
+
 
   return (
     <>
       <div className='flex flex-wrap flex-col items-center mt-10'>
         <h1 className='text-4xl font-medium text-indigo-800'>Currency Converter</h1>
-        <div className='mt-16 p-10 bg-indigo-300 max-w-xl rounded-2xl'>
+        <div className='mt-16 p-10 bg-indigo-200 max-w-xl rounded-2xl'>
            <form onSubmit={(e) =>{
             e.preventDefault();
             convert();
-           }}>
+           }}
+           className='flex flex-col items-center'>
             <div className='w-full m-2'>
               <InputBox
-                label = 'from'
-                amount = {amount}
+                label = 'From'
+                amountValue = {amount}
                 currencyOptions = {options}
                 onCurrencyChange = {(currency) => setFrom(currency)}
                 onAmountChange = {(amount) => setAmount(amount)}
                 selectedCurrency = {from}
               />
             </div>
+            <button className='text-2xl my-1 px-6 py-2 rounded-xl bg-indigo-600 text-white' onClick={()=>{
+              swap();
+              convert();
+            }}>Swap</button>
+            <div className='w-full m-2'>
+              <InputBox
+                label = 'To'
+                amountValue = {convertedAmount}
+                currencyOptions = {options}
+                onCurrencyChange = {(currency) => setTo(currency)}
+                selectedCurrency = {to}
+                amountDisabled={true}
+              />
+            </div>
+            <button type="submit"
+            className='mt-8 px-8 py-3 rounded-xl bg-indigo-600 text-white text-2xl cursor-pointer'>
+              Convert   {from.toUpperCase()}   to   {to.toUpperCase()}
+           </button>
            </form>
         </div>
       </div>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React, { useId } from 'react'
 
 function InputBox({
   label,
-  amount,
+  amountValue,
   onAmountChange,
   onCurrencyChange,
   currencyOptions = [],
   selectedCurrency = "usd",
   currencyDisabled = false,
+  amountDisabled = false
 }) {
+  const id = useId();
   return (
     <>
       <div className='bg-white p-5 text-xl rounded-xl flex'>
         <div className='w-1/2'>
-          <label className='text-black/60 mb-4 inline-block'>{label}</label>
-          <input type="number"
+          <label htmlFor={id} className='text-black/60 mb-4 inline-block'>{label}</label>
+          <input type= 'number'
+            id={id}
             placeholder='Amount'
-            value={amount}
+            value={amountValue}
             onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))} 
             className='p-1 rounded-lg outline-none'
+            disabled={amountDisabled}
           />
         </div>
         <div className='w-1/2 flex flex-wrap justify-end text-right'>
@@ -26,7 +30,6 @@ function InputBox({
           <select className='rounded-lg cursor-pointer outline-none bg-gray-100 p-1'
             value={selectedCurrency}
             onChange={(e) => { onCurrencyChange && onCurrencyChange(e.target.value) }}
-            disabled={currencyDisabled}
           >
             {currencyOptions.map((currency) => (
               <option key={currency} value={currency}>{currency}</option>
